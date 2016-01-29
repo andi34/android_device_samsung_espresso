e7cb34cef6636ee720a44d44a18981039bb97e75
Ziyan <jaraidaniel@gmail.com>
2016-01-29 19:19:51 +0100

    Set up unified build for wifi-only variants
    Change-Id: I9fffe523aefac96e2cd0461b9398806b02e4c131

---
+++ omni_espresso.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Bootanimation
+TARGET_BOOTANIMATION_SIZE := 480x320
+
+# Inherit common Omni configuration
+$(call inherit-product, vendor/omni/config/common_tablet.mk)
+
+# OmniRom specific overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/espresso/overlay/custom-common
+
+# Inherit device specific configuration
+$(call inherit-product, device/samsung/espresso/aosp_espresso.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_espresso
