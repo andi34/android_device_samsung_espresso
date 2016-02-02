159b3f36170ae2ed7cfaf30b0cb96674aace149e
Andreas Blaesius <skate4life@gmx.de>
2016-02-02 10:24:42 +0100

    espresso-common: don't include recovery files in boot.img
    Change-Id: I2b7f7bae22bc28216fd079b39c6b64dce920de2e

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -83,7 +83,7 @@ LOCAL_MODULE       := init.recovery.espresso.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ../recovery/root/init.recovery.espresso.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
 endif
 
@@ -92,7 +92,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := init.recovery.usb.rc
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
 LOCAL_SRC_FILES := ../recovery/root/init.recovery.usb.rc
 include $(BUILD_PREBUILT)
 endif
